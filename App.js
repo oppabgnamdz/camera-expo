6c00ac0602ca1f6d8bf3ccabaf2aa0dfcf8213ae
nam <namdeptrai@nams-MacBook-Pro.local>
2020-10-15 18:09:10 +0700

    finish

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Camera } from 'expo-camera';
+import { FontAwesome } from '@expo/vector-icons'
+import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from 'expo-permissions'
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const camRef = useRef(null);
+  const takeAPicture = async () => {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      if (data.uri) {
+        handleSave(data.uri)
+      }
+    }
+  }
+  const handleSave = async (path) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === 'granted') {
+      console.log('have image')
+      const assert = await MediaLibrary.createAssetAsync(path);
+      MediaLibrary.createAlbumAsync('tutorial Camera', assert)
+    } else {
+      console.log('oops')
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <TouchableOpacity style={styles.button} onPress={takeAPicture}>
+        <FontAwesome name="camera" size={23} color="#FFF" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +81,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    margin: 20,
+    borderRadius: 10,
+    height: 50
+  }
 });
